f792d95c5d2769815a9fcbdd8121bc4d9448cfd3
Alexey Sur <alexeysur@gmail.com>
2021-06-21 14:19:43 +0300

    feat: solution for exercices 1 and 2

--- Swift_Task6/ios.stage-task/Exercises/Exercise-2/Game.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-2/Game.swift
@@ -18,6 +18,22 @@ struct Game: GameCompatible {
 extension Game {
 
     func defineFirstAttackingPlayer(players: [Player]) -> Player? {
-        nil
+        var firtsPlayer = Player()
+        let values = [Value.six, .seven, .eight, .nine, .ten, .jack, .seven, .king, .ace]
+        
+        
+        var tmp = 8
+        for player in players {
+            for index in  0 ..< 6 {
+                if ((player.hand?[index].isTrump) == true) {
+                    let indexCard = values.firstIndex(where: {$0 == player.hand?[index].value})!
+                    if  indexCard <= tmp {
+                        tmp = indexCard
+                        firtsPlayer = player
+                    }
+                }
+            }
+        }
+        return firtsPlayer
     }
 }
